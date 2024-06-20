698c3826e68f9444b320be9e6406d88d57874fff
LiviaLausch <livialausch1234@gmail.com>
2024-06-20 16:26:28 -0300

    updated

--- src/database/knex/migrations/20240615232634_createMoviesNotes.js
+++ src/database/knex/migrations/20240615232634_createMoviesNotes.js
@@ -11,25 +11,3 @@ exports.up = knex => knex.schema.createTable("notes", table => {
 })
 
 exports.down = knex => knex.schema.dropTable("notes");
-
-knex('users').where({ id: 5 }).first()
-  .then(user => {
-    if (user) {
-      // Inserir na tabela "notes" porque o usuário existe
-      return knex('notes').insert({
-        title: 'Filme 1',
-        description: 'Descrição do filme',
-        rating: 8,
-        user_id: 5
-      });
-    } else {
-      // Tratar o caso onde o usuário não existe
-      throw new Error('Usuário não encontrado');
-    }
-  })
-  .then(() => {
-    console.log('Nota inserida com sucesso');
-  })
-  .catch(error => {
-    console.error('Erro ao inserir nota:', error);
-  });
